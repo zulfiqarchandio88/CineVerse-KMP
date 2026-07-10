37cfc601eecee74c0e742053fff602cb79c2df02
Admin <zulfiqar.chandio88@gmail.com>
2026-07-10 14:50:08 +0500

    added ios required changes

--- androidApp/src/main/kotlin/com/zulfiqar/cineverse/CineVerseApp.kt
+++ androidApp/src/main/kotlin/com/zulfiqar/cineverse/CineVerseApp.kt
@@ -2,13 +2,13 @@ package com.zulfiqar.cineverse
 
 import android.app.Application
 import com.zulfiqar.cineverse.core.platform.UrlLauncher
-import com.zulfiqar.cineverse.di.initKoin
+import com.zulfiqar.cineverse.di.doInitKoin
 
 class CineVerseApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
         UrlLauncher.context = this
-        initKoin()
+        doInitKoin()
     }
 }
